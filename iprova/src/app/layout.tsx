68d23863f57ef88c0d2ba903d637987f6332b5c8
RobertoZapata <robertozapataz25@gmail.com>
2024-01-23 05:07:02 -0500

    metada info

--- iprova/src/app/layout.tsx
+++ iprova/src/app/layout.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import { Metadata } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
+
+export const metadata: Metadata = {
+  title: 'iProva',
+  description: 'Created by create next app',
+};
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
