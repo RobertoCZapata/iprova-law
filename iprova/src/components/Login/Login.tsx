3cd47a6625d916a305a4296a7618e2d8810419bb
RobertoZapata <robertozapataz25@gmail.com>
2024-01-24 06:01:33 -0500

    input the logo in the login page

--- iprova/src/components/Login/Login.tsx
+++ iprova/src/components/Login/Login.tsx
@@ -6,13 +6,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import Card from '@mui/material/Card';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CardMedia  from '@mui/material/CardMedia';
 
 function Copyright(props: any) {
   return (
@@ -47,32 +48,36 @@ const Login = () => {
             flexDirection: 'column',
             alignItems: 'center',
           }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
+         <Card sx={{ minWidth: 345 }}>
+          <CardMedia
+          sx={{ height: 140 }}
+          image="/logo.jpg"
+          title='iprova'
+          />
+          </Card>
           <Typography component='h1' variant='h5'>
-            Sign in
+            Iniciar Sesion
           </Typography>
           <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField margin='normal' required fullWidth id='email' label='Email Address' name='email' autoComplete='email' autoFocus />
+            <TextField margin='normal' required fullWidth id='email' label='Correo Electronico' name='email' autoComplete='email' autoFocus />
             <TextField
               margin='normal'
               required
               fullWidth
               name='password'
-              label='Password'
+              label='Contraseña'
               type='password'
               id='password'
               autoComplete='current-password'
             />
-            <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
+            <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Recordarme' />
             <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-              Sign In
+              Ingresar
             </Button>
             <Grid container>
               <Grid item xs>
                 <Link href='#' variant='body2'>
-                  Forgot password?
+                  Olvisdaste la Contraseña?
                 </Link>
               </Grid>
               <Grid item>
