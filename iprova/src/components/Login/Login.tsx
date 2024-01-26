77538e1aa1143f2fc59a50913558729b1faae10b
RobertoZapata <robertozapataz25@gmail.com>
2024-01-26 05:31:21 -0500

    solving PRs comments, copyright component and blanks spaces

--- iprova/src/components/Login/Login.tsx
+++ iprova/src/components/Login/Login.tsx
@@ -1,6 +1,5 @@
 'use client';
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -10,23 +9,13 @@ import Card from '@mui/material/Card';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import CardMedia  from '@mui/material/CardMedia';
+import CardMedia from '@mui/material/CardMedia';
+import Copyright from '../Copyright/Copyright';
+import { grey } from '@mui/material/colors';
 
-function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+const color = grey[800];
 
 const Login = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -39,58 +28,49 @@ const Login = () => {
   };
 
   return (
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-         <Card sx={{ minWidth: 345 }}>
-          <CardMedia
-          sx={{ height: 140 }}
-          image="/logo.jpg"
-          title='iprova'
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Card sx={{ minWidth: 345, mb: 2 }}>
+          <CardMedia sx={{ height: 240 }} image='/logo.jpg' title='iprova' />
+        </Card>
+        <Typography component='h1' variant='h5' color={color} >
+          Iniciar Sesion
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField margin='normal' required fullWidth id='email' label='Correo Electronico' name='email' autoComplete='email' autoFocus />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Contraseña'
+            type='password'
+            id='password'
+            autoComplete='current-password'
           />
-          </Card>
-          <Typography component='h1' variant='h5'>
-            Iniciar Sesion
-          </Typography>
-          <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField margin='normal' required fullWidth id='email' label='Correo Electronico' name='email' autoComplete='email' autoFocus />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label='Contraseña'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Recordarme' />
-            <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-              Ingresar
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Olvisdaste la Contraseña?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='#' variant='body2'>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+          <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Recordarme' />
+          <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+            Ingresar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                Olvisdaste la Contraseña?
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
-}
+};
 
-  export default Login;
+export default Login;
